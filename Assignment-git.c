2ddbf9e420cc65669c2f32de27196df71f48d1b0
Anajak <wedoza112@gmail.com>
2026-03-15 22:57:23 +0700

    add calculator program

---
+++ Assignment-git.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    float a, b;
+    char op;
+
+    while (1) {
+        scanf("%f%c%f", &a, &op, &b);
+
+        switch (op) {
+            case '+':
+                printf("= %.2f\n\n", a + b);
+                break;
+            case '-':
+                printf("= %.2f\n\n", a - b);
+                break;
+            case '*':
+                printf("= %.2f\n\n", a * b);
+                break;
+            case '/':
+                if (b != 0)
+                    printf("= %.2f\n\n", a / b);
+                else
+                    printf("= หารรรรรรด้วยศูนย์ไม่ได้\n\n");
+                break;
+            default:
+                printf("= เครื่องหมายยยยไม่ถูกต้อง\n\n");
+        }
+    }
+printf("Calculator Program\n");
+    return 0;
+}
